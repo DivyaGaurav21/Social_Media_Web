07b8b2d8477dafe45558d93c8afc7f664f2aa922
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-11-15 18:26:17 +0530

    setup router and controller of project

--- routers/index.js
+++ routers/index.js
@@ -2,3 +2,14 @@
 const express = require('express');
 
 // setup express router 
+const router = express.Router();
+
+//controller for routing
+const homeController = require('../controllers/home-controller');
+
+//check whether router is loaded or not
+console.log('Router is Loaded');
+
+router.get('/' , homeController.home);
+
+module.exports = router;
